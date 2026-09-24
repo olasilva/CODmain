e22b4ed425200ee4212eb7a67d8a41990fcf40fa
olasilva <olasilvaolunleke>
2026-09-24 15:43:30 +0100

    admin layout

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -7,7 +7,7 @@ export default function AdminLayout() {
   return (
     <div className="min-h-screen flex bg-[#F5F9FF]">
       <AdminSidebar />
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex-1 flex flex-col min-w-0 ml-[300px]">
         <AdminHeader />
         <main className="flex-1 p-6 overflow-auto">
           <Outlet />
